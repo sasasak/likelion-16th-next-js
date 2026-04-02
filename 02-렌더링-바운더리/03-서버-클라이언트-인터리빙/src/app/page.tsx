a6e62b1e104427edc3c7249945209f4c3011fcea
sasasak <geun02young@naver.com>
2026-04-02 11:44:43 +0900

    learn. Activity 컴포넌트 - React v19.2.0에 추가된 새로운 컴포넌트 - Vue.js의 v-if vs v-show 비교한 것과 유사

--- 02-렌더링-바운더리/03-서버-클라이언트-인터리빙/src/app/page.tsx
+++ 02-렌더링-바운더리/03-서버-클라이언트-인터리빙/src/app/page.tsx
@@ -5,7 +5,6 @@ import Cart from '@/components/cart' // 서버 컴포넌트
 import Modal from '@/components/modal' // 클라이언트 컴포넌트
 
 export default function Page() {
-
   // 서버 컴포넌트 또는 클라이언트 컴포넌트에 서로를 포함해보세요.
   // 어떤 일이 일어나는지 확인하고, 문제가 발생한다면 해결 방법도 살펴봅시다.
 
