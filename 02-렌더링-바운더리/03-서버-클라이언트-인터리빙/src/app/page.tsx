7f69e0667bc1b19724f985f19c04dfcb95771d03
sasasak <geun02young@naver.com>
2026-04-02 14:02:16 +0900

    config. 인터리빙 데모(Interleaving Demo) 내보내기 설정

--- 02-렌더링-바운더리/03-서버-클라이언트-인터리빙/src/app/page.tsx
+++ 02-렌더링-바운더리/03-서버-클라이언트-인터리빙/src/app/page.tsx
@@ -1,8 +1,7 @@
-import { ClientComponent, ServerComponent } from '@/_learn'
-
 import { cn } from '@/utils'
-import Cart from '@/components/cart' // 서버 컴포넌트
-import Modal from '@/components/modal' // 클라이언트 컴포넌트
+import { ClientComponent, InterleavingDemo, ServerComponent } from '@/_learn'
+// import Cart from '@/components/cart' // 서버 컴포넌트
+// import Modal from '@/components/modal' // 클라이언트 컴포넌트
 
 export default function Page() {
   // 서버 컴포넌트 또는 클라이언트 컴포넌트에 서로를 포함해보세요.
@@ -34,11 +33,13 @@ export default function Page() {
       </header>
 
       <main className="flex flex-col gap-5 overflow-hidden transform-3d">
+        <InterleavingDemo />
+
         {/* 클라이언트 컴포넌트 (동기 처리, 이벤트 핸들링, 상태 관리 등) */}
-        <Modal>
-          {/* 서버 컴포넌트 (비동기 처리, 사전 렌더링, 서버 데이터 가져오기 등) */}
-          <Cart />
-        </Modal>
+        {/* <Modal> */}
+        {/* 서버 컴포넌트 (비동기 처리, 사전 렌더링, 서버 데이터 가져오기 등) */}
+        {/* <Cart /> */}
+        {/* </Modal> */}
 
         <ClientComponent hidden>
           {/* <slot></slot> <- props.children */}
